cb88ed59c5afe5496ab679557fa6f0a3148f40b7
piyush-mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2024-05-10 20:02:48 +0530

    Database created and models

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,7 @@
-const Home = () => {
-  return (
-    <div>
-      <p>Home</p>
-    </div>
-  );
+import React from "react";
+
+const page = () => {
+  return <div>Home </div>;
 };
 
-export default Home;
+export default page;
